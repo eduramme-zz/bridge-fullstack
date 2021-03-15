c7df55e93a9c1916b8b828a38802015d695b2950
Eduardo Lennert Rammé <eduramme@gmail.com>
2021-03-15 12:34:31 -0300

    create basic design and architecture

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+
+const Input = styled.input`
+  padding: 0.5em;
+  margin: 0.5em;
+  color: "palevioletred";
+  background: white;
+  border: none;
+  border-radius: 3px;
+`;
+
+const Button = styled.button`
+  padding: 0.5em;
+  margin: 0.5em;
+  color: "palevioletred";
+  background: white;
+  border: none;
+  border-radius: 3px;
+`;
+
+
+const Wrapper = styled.div`
+  padding: 10px 0 ;
+
+`
+
+const Title = styled.h1`
+  padding: 10px 0;
+`
+
+const Lista = styled.p`
+  padding: 10px 0;
+`
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title>Calcule seus números primos</Title>
+      <Wrapper>
+        <Input></Input>
+        <Input></Input>
+        <Button>Cálcular</Button>
+      </Wrapper>
+      <Lista>Lista: números primos: 2, 3, 5, 7...</Lista>
     </div>
   );
 }
