ebb4cdfb648f29246635cbec39dc32b2c79f8287
Eduardo Lennert Rammé <eduramme@gmail.com>
2021-03-16 00:19:45 -0300

    create basic functionalities

--- Client/src/App.js
+++ Client/src/App.js
@@ -38,21 +38,44 @@ const Lista = styled.p`
 
 function App() {
   const [loading, setLoading] = useState(false);
+  const [primes, setPrimes] = useState(null);
+  const [min, setMin] = useState();
+  const [max, setMax] = useState();
+
+  console.log(min, max)
+
+  const handleClick = async () => {
+    if(loading){
+      return;
+    }
+    setLoading(true);
+    console.log(`/primes/${min}/${max}`);
+    let response = await fetch(`/primes/${min}/${max}`);
+
+    if (response.ok) {
+      let json = await response.json();
+      setPrimes(json.primes);
+      console.log(json)
+    } else {
+      alert("HTTP-Error: " + response.status);
+    }
+    setLoading(false);
+  }
 
   return (
     <div className="App">
         <Title>Calcule seus números primos</Title>
         <Wrapper>
-          <Input></Input>
-          <Input></Input>
-          <Button onClick={()=>{console.log('button pressed')}}>{
+          <Input type='number' onChange={(e) => {setMin(parseInt(e.target.value))}}></Input>
+          <Input type='number' onChange={(e) => {setMax(parseInt(e.target.value))}}></Input>
+          <Button onClick={()=>{handleClick()}}>{
             loading ?
-            'loading'
+            <div class="lds-dual-ring"></div>
             :
             'Cálcular'
           }</Button>
         </Wrapper>
-        <Lista>Lista de números primos: 2, 3, 5, 7...</Lista>      
+        <Lista>Lista de números primos: {primes ? primes.join(', ') : ''}</Lista>      
     </div>
   );
 }
